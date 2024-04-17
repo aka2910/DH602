890cd0cb94ead2ecf6fb19b87349a2253dd18d31
Akshat Kumar <81357954+aka2910@users.noreply.github.com>
2024-04-17 20:14:51 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,49 @@
+import pickle
+import torch
+import numpy as np
+from torchvision import transforms
+
+
+
+results = pickle.load(open('results.obj','rb'))
+labels = pickle.load(open('labels.obj','rb'))
+print(len(labels)), print(len(results))
+
+
+# An example with a single image
+# results has all the images tiled into patches
+sz = 256
+N = 16
+img_res = [results[0][i]['img'] for i in range(N)]
+img_res_tensor = torch.tensor(np.array(img_res)).view((N,3,sz,sz)) # torch.Size([16, 3, 128, 128])
+
+
+# Define transformation to apply to the image
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),  # Resize to match the input size expected by the model
+])
+
+# Apply transformation to the image
+image_tensor = transform(img_res)
+
+
+model = torch.load("ensemble.pth")
+
+with model.eval():
+    out = model(image_tensor.float())
+
+## Do for all images
+# create a tensor of all the patches for all the images
+# pass through the model
+# get the output
+
+# create a tensor of all the patches for all the images using results
+images = []
+for i in range(len(results)):
+    img_res = [results[i][j]['img'] for j in range(N)]
+    img_res_tensor = torch.tensor(np.array(img_res)).view((N,3,sz,sz)) # torch.Size([16, 3, 128, 128])
+    images.append(img_res_tensor)
+
+images = torch.stack(images)
+output = model(images.float())
+print(output)
